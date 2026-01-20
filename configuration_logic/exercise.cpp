1d9c10b0d7d9ff437055efbc697c1568e957b90d
ybenchel <yass515bec@outlook.fr>
2026-01-20 17:35:28 +0100

    tokenazation part

--- configuration_logic/exercise.cpp
+++ configuration_logic/exercise.cpp
@@ -1,104 +1,115 @@
 #include  <iostream>
 #include  <fstream>
 #include  <deque>
+#include <cctype>
 
-enum NodeType { 
-    ROOT, 
-    SERVER, 
-    LOCATION 
+enum TokenType {
+    KEYWORD,
+    WORD,
+    SYMBOL
 };
 
-class nodes
+struct Token
 {
-    private:
-        nodes *parent;
-        NodeType type;
-        std::string name;
-        std::deque<std::string> values;
-        std::deque<nodes> children;
-    public:
-        nodes() : parent(nullptr) {}
-        void setType(NodeType type)
-        {
-            this->type = type;
-        }
-        void setName(std::string name)
-        {
-            this->name = name;
-        }
-        void addChild(nodes& child)
-        {
-            children.push_back(child);
-            children.back().parent = this; 
-        }
-        void addVal(std::string val)
-        {
-            values.push_back(val);
-        }
-        nodes* getPrevChild()
-        {
-            return parent;
-        }
-        nodes* getLastChild()
-        {
-            if (children.empty())
-                return nullptr;
-            return &children.back();
-        }
-        void printval()
-        {
-            for(size_t i = 0; i < values.size(); i++)
-            {
-                std::cout << name << std::endl;
-                std::cout << values[i] << std::endl;
-            }
-        }
+    TokenType type;
+    std::string value;
+    int line; 
 };
 
-void    treeing(std::string line, nodes **current)
-{
-    int pos = 0;
-
-    if ((pos = line.find("server")) != std::string::npos)
+void identifying_words_and_keywords(std::string& tok, std::deque<Token>& tokenContainer, int Line)
     {
-        nodes server;
-        server.setType(SERVER);
-        server.setName("server");
-        (*current)->addChild(server);
-        *current = (*current)->getLastChild();
-    }else if ((pos = line.find("location")) != std::string::npos)
-    {
-        nodes location;
-        location.setType(LOCATION);
-        location.setName("location");
-        (*current)->addChild(location);
-        *current = (*current)->getLastChild();
-    }else if ((pos = line.find("}")) != std::string::npos)
-    {
-        if ((*current)->getPrevChild() != nullptr)
-            *current = (*current)->getPrevChild();
-    }else
-    {
-        (*current)->addVal(line);
+        if (tok == "server" || tok == "location" || tok == "listen"
+            || tok == "root" || tok == "index" || tok == "allow_methods" || tok == "autoindex"
+             || tok == "cgi_pass" || tok == "error_page" || tok == "client_max_body_size")
+        {
+            Token tikken;
+            tikken.type = KEYWORD;
+            tikken.value.append(tok);
+            tikken.line = Line;
+            tokenContainer.push_back(tikken);
+            tok.clear();
+        }else
+        {
+            Token tikken;
+            tikken.type = WORD;
+            tikken.value.append(tok);
+            tikken.line = Line;
+            tokenContainer.push_back(tikken);
+            tok.clear();
+        }
     }
+
+void tokenzation(std::string fileContent)
+{
+    std::string tok;
+    size_t Line;
+    size_t pos;
+    std::deque<Token> tokenContainer;
+
+    Line = 1;
+    for(size_t i = 0; i < fileContent.size(); i++)
+    {
+        if (fileContent[i] == '\n')
+        {
+            if (!tok.empty())
+                identifying_words_and_keywords(tok, tokenContainer, Line);
+            Line++;
+        }
+        else if (fileContent[i] == ' ' || fileContent[i] == '\t')
+        {
+            if (!tok.empty())
+                identifying_words_and_keywords(tok, tokenContainer, Line);
+        }else if ((fileContent[i] == ';' || fileContent[i] == '{' || fileContent[i] == '}'))
+        {
+            if (!tok.empty())
+                identifying_words_and_keywords(tok, tokenContainer, Line);
+            Token tikken;
+            tikken.type = SYMBOL;
+            tikken.value.push_back(fileContent[i]);
+            tikken.line = Line;
+            tokenContainer.push_back(tikken);
+            tok.clear();
+        }
+        else
+            tok.push_back(fileContent[i]);
+    }
+    if (!tok.empty())
+    {
+        if (tok == "server" || tok == "location")
+        {
+            Token tikken;
+            tikken.type = KEYWORD;
+            tikken.value.append(tok);
+            tikken.line = Line;
+            tokenContainer.push_back(tikken);
+            tok.clear();
+        }else
+        {
+            Token tikken;
+            tikken.type = WORD;
+            tikken.value.append(tok);
+            tikken.line = Line;
+            tokenContainer.push_back(tikken);
+            tok.clear();
+        }
+    }
+    for(size_t i = 0; i < tokenContainer.size(); i++)
+        std::cout << tokenContainer[i].type << "   " << tokenContainer[i].value << std::endl;
 }
 
-int main()
+int main(int ac, char **av)
 {
-    std::ifstream infile("sub");
+    if (ac < 2)
+    {
+        std::cout << "Error: file is missing" << std::endl;
+        return 1;
+    }
+    std::ifstream infile(av[1]);
     if (infile.fail())
     {
-        std::cout << "Error" << std::endl;
+        std::cout << "Error: can't open file!" << std::endl;
+        return 1;
     }
-    std::string line;
-    nodes root;
-    root.setType(ROOT);
-    root.setName("root");
-    nodes* current = &root;
-    while(getline(infile, line))
-    {
-        treeing(line, &current);
-        current->printval();
-        std::cout << "-----------" << std::endl;
-    }
+    std::string fileContent((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());
+    tokenzation(fileContent);
 }
