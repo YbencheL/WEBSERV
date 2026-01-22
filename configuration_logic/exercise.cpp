e9001cd6d798c45fe154539b0fda16718b919d78
ybenchel <yass515bec@outlook.fr>
2026-01-22 11:42:13 +0100

    i dont know what the fuck im doing anymore

--- configuration_logic/exercise.cpp
+++ configuration_logic/exercise.cpp
@@ -3,6 +3,8 @@
 #include  <deque>
 #include <cctype>
 #include <sstream>
+#include <map>
+#include <algorithm>
 
 enum TokenType {
     KEYWORD,
@@ -17,35 +19,61 @@ struct Token
     int line; 
 };
 
+struct LocationBlock
+{
+
+};
+
+struct ServerBlock
+{
+    int listen;
+    std::string root;
+    std::string host;
+    std::string server_name;
+    int client_max_body_size;
+    std::deque<std::string> index;
+    std::map<int, std::string> error_page;
+    // std::deque<LocationBlock> locations;
+};
+
+void error_line(std::string msg, int Line)
+{
+    std::string errorLine;
+    std::stringstream ss;
+
+    ss << Line;
+    errorLine = "ERROR on line " + ss.str() + msg;
+    throw std::runtime_error(errorLine);
+}
+
 void identifying_words_and_keywords(std::string& tok, std::deque<Token>& tokenContainer, int Line)
+{
+    if (tok == "server" || tok == "location" || tok == "listen" || tok == "host"
+        || tok == "root" || tok == "index" || tok == "allow_methods" || tok == "autoindex"
+            || tok == "cgi_pass" || tok == "error_page" || tok == "client_max_body_size")
     {
-        if (tok == "server" || tok == "location" || tok == "listen"
-            || tok == "root" || tok == "index" || tok == "allow_methods" || tok == "autoindex"
-             || tok == "cgi_pass" || tok == "error_page" || tok == "client_max_body_size")
-        {
-            Token tikken;
-            tikken.type = KEYWORD;
-            tikken.value.append(tok);
-            tikken.line = Line;
-            tokenContainer.push_back(tikken);
-            tok.clear();
-        }else
-        {
-            Token tikken;
-            tikken.type = WORD;
-            tikken.value.append(tok);
-            tikken.line = Line;
-            tokenContainer.push_back(tikken);
-            tok.clear();
-        }
+        Token tikken;
+        tikken.type = KEYWORD;
+        tikken.value.append(tok);
+        tikken.line = Line;
+        tokenContainer.push_back(tikken);
+        tok.clear();
+    }else
+    {
+        Token tikken;
+        tikken.type = WORD;
+        tikken.value.append(tok);
+        tikken.line = Line;
+        tokenContainer.push_back(tikken);
+        tok.clear();
     }
+}
 
 void is_syntax_valid(std::deque<Token> tokenContainer)
 {
     size_t keepCountOfBrase = 0;
+    size_t ServerBlockCount = 0;
     bool inside = false;
-    std::string errorLine;
-    std::stringstream ss;
 
     for (int i = 0; i < tokenContainer.size(); i++)
     {
@@ -61,30 +89,111 @@ void is_syntax_valid(std::deque<Token> tokenContainer)
             {
                 keepCountOfBrase--;
                 if (keepCountOfBrase == 0)
+                {
                     inside = false;
-            }
+                    ServerBlockCount = 0;
+                }
+            }else if ((i - 1) >= 0 && tokenContainer[i].value == ";" && tokenContainer[i - 2].value == ";" )
+                error_line(": syntax error related to ;", tokenContainer[i].line);
         }
         else if (tokenContainer[i].type == 1)
-        {            
-            if ((i - 1) >= 0 && tokenContainer[i - 1].value != "location" && tokenContainer[i + 1].value != ";")
-            {
-                ss << tokenContainer[i].line;
-                errorLine = "ERROR on line " + ss.str() + ": directives must end with ;";
-                throw std::runtime_error(errorLine);
-            }
+        {
+            if ((i - 1) >= 0 && tokenContainer[i - 1].value != "location" && tokenContainer[i + 1].type != 1 && tokenContainer[i + 1].value != ";")
+                error_line(": directives must end with ;", tokenContainer[i].line);
+            if (!inside)
+                error_line(": configuration must be done inside a server block", tokenContainer[i].line);
         }
         else if (tokenContainer[i].type == 0)
         {
-            if (tokenContainer[i].value == "location" && !inside)
-            {
-                ss << tokenContainer[i].line;
-                errorLine = "ERROR on line " + ss.str() + ": location block must be inside the server block";
-                throw std::runtime_error(errorLine);
-            }
+            if ((tokenContainer[i].value == "server" && tokenContainer[i + 1].value != "{") ||
+                    (tokenContainer[i].value == "location" && tokenContainer[i + 2].value != "{"))
+                error_line(": server and location block must be followed with braces", tokenContainer[i].line);
+            else if (tokenContainer[i].value == "server")
+                ServerBlockCount++;
+
+            else if (tokenContainer[i].value == "location" && !inside)
+                error_line(": location block must be inside the server block", tokenContainer[i].line);
+
+            else if (ServerBlockCount > 1)
+                error_line(": nested server blocks isn't allowed!", tokenContainer[i].line);
         }
     }
     if (keepCountOfBrase != 0)
         throw std::runtime_error("ERROR: check brackets!");
+}
+
+void duplicate_check(std::deque<std::string>& keywords, std::string name)
+{
+    int count = 0;
+    
+    count = std::count(keywords.begin(), keywords.end(), name);
+    if (count > 1)
+        throw std::runtime_error("ERROR: there must be no duplicates for these keywords (listen client_max_body_size server_name error_page)");
+}
+
+void extracting_server_config(std::deque<Token>& tokenContainer)
+{
+    // bool insideServerBlock = false;
+    ServerBlock Serv;
+    std::deque<std::string> keywords;
+
+    // duplicate check rule
+    for (int i = 0; i < tokenContainer.size(); i++)
+    {
+        if (tokenContainer[i].type == 0)
+            keywords.push_back(tokenContainer[i].value);
+    }
+    duplicate_check(keywords, "listen");
+    duplicate_check(keywords, "client_max_body_size");
+    duplicate_check(keywords, "server_name");
+    duplicate_check(keywords, "error_page");
+    // storing values
+    for (int i = 0; i < tokenContainer.size(); i++)
+    {
+        if (tokenContainer[i].type == 0)
+        {
+            if (tokenContainer[i].value == "listen")
+            {
+                std::stringstream ss(tokenContainer[i + 1].value);
+                ss >> Serv.listen;
+            }
+            else if (tokenContainer[i].value == "host")
+                Serv.host = tokenContainer[i + 1].value;
+            else if (tokenContainer[i].value == "root")
+                Serv.root = tokenContainer[i + 1].value;
+            else if (tokenContainer[i].value == "server_name")
+                Serv.server_name = tokenContainer[i + 1].value;
+            else if (tokenContainer[i].value == "client_max_body_size")
+            {
+                std::stringstream ss(tokenContainer[i + 1].value);
+                ss >> Serv.client_max_body_size;
+            }
+            else if (tokenContainer[i].value == "error_page")
+            {
+                int errornum = 0;
+                std::stringstream ss(tokenContainer[i + 1].value);
+                ss >> errornum;
+                Serv.error_page.insert(std::make_pair(errornum, tokenContainer[i + 2].value));
+            }
+            else if (tokenContainer[i].value == "index")
+            {
+                i++;
+                while(tokenContainer[i].type == 1)
+                {
+                    Serv.index.push_back(tokenContainer[i].value);
+                    i++;
+                }
+            }
+        }
+    }
+    if (Serv.server_name.empty())
+        Serv.server_name = "WEBSERV";
+    if (Serv.host.empty())
+        Serv.host = "127.0.0.1";
+    if (Serv.index.empty())
+        Serv.index.push_back("index.html");
+    if (!Serv.listen || Serv.root.empty() || Serv.error_page.empty()|| !Serv.client_max_body_size)
+        throw std::runtime_error("ERROR: missing value!");
 }
 
 void tokenzation(std::string fileContent)
@@ -97,13 +206,18 @@ void tokenzation(std::string fileContent)
     Line = 1;
     for(size_t i = 0; i < fileContent.size(); i++)
     {
+        if (fileContent[i] == '#')
+        {
+            while(fileContent[i] != '\n')
+                i++;
+        }
         if (fileContent[i] == '\n')
         {
             if (!tok.empty())
                 identifying_words_and_keywords(tok, tokenContainer, Line);
             Line++;
         }
-        else if (fileContent[i] == ' ' || fileContent[i] == '\t')
+        else if ((fileContent[i] == ' ' || fileContent[i] == '\t'))
         {
             if (!tok.empty())
                 identifying_words_and_keywords(tok, tokenContainer, Line);
@@ -122,26 +236,9 @@ void tokenzation(std::string fileContent)
             tok.push_back(fileContent[i]);
     }
     if (!tok.empty())
-    {
-        if (tok == "server" || tok == "location")
-        {
-            Token tikken;
-            tikken.type = KEYWORD;
-            tikken.value.append(tok);
-            tikken.line = Line;
-            tokenContainer.push_back(tikken);
-            tok.clear();
-        }else
-        {
-            Token tikken;
-            tikken.type = WORD;
-            tikken.value.append(tok);
-            tikken.line = Line;
-            tokenContainer.push_back(tikken);
-            tok.clear();
-        }
-    }
+        identifying_words_and_keywords(tok, tokenContainer, Line);
     is_syntax_valid(tokenContainer);
+    extracting_server_config(tokenContainer);
     for(size_t i = 0; i < tokenContainer.size(); i++)
         std::cout << tokenContainer[i].value << std::endl;
 }
