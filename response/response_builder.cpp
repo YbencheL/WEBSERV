7153d02199c7d9950365229e7896410bcdcca815
Youness Anflous <anayouness51@gmail.com>
2026-03-05 00:40:20 +0000

    feat: implement DELETE method with path normalization and security checks
    - Added ft_delete with stat() and unlink() system calls
    - Implemented S_ISDIR check to prevent directory deletion (403 Forbidden)
    - Added path normalization to collapse double slashes
    - Configured 204 No Content response for successful deletion
    - Added custom 404/403 error page integration

--- response/response_builder.cpp
+++ response/response_builder.cpp
@@ -1,5 +1,4 @@
 # include "../response_builder.hpp"
-// # include "../socket_engine.hpp"
 # include "../utils/utils.hpp"
 # include "../client.hpp"
 # include <stack>
@@ -10,14 +9,14 @@ void    response_builder::init_response_builder(Client &current_client) {
     this->current_client = &current_client;
 }
 
-bool    response_builder::is_allowd_method(std::string method)
-{
-    for (size_t i = 0; i < current_client->location_conf->allow_methods.size(); i++) {
-        if (current_client->location_conf->allow_methods.at(i) == method)
-            return (true);
-    }
-    return (false);
-}
+// bool    response_builder::is_allowd_method(std::string method)
+// {
+//     for (size_t i = 0; i < current_client->location_conf->allow_methods.size(); i++) {
+//         if (current_client->location_conf->allow_methods.at(i) == method)
+//             return (true);
+//     }
+//     return (false);
+// }
 
 std::string response_builder::index_file_iterator(const std::string &full_path)
 {
@@ -34,33 +33,29 @@ std::string response_builder::index_file_iterator(const std::string &full_path)
     return ("");
 }
 
-void response_builder::response_setup()
-{
-    std::cout << "STATUS CODE " << current_client->res.get_stat_code() << std::endl;
-    if (current_client->res.get_stat_code() != OK)  // error page
-    {
-        generate_error_page();
-    }
-    else if (current_client->req.getMethod() == GET_METHODE)
-        handle_get();
-    else if (current_client->req.getMethod() == POST_METHODE)
-        handle_post();
-    else if (current_client->req.getMethod() == DELETE_METHODE)
-        handle_delete();
-    this->current_client->res.set_raw_response(response_holder);
-    
-    std::cout << "--------- START RESPONSE\n" << response_holder << "\n------- END RESPONSE" << std::endl;
-
-}
-
+// TODO-LATER: Methode not allowed
 void response_builder::build_response()
 {
-    // exit(1);
-    if (this->current_client->res.get_stat_code() == OK) {
-        if (!is_allowd_method(current_client->req.getMethod()))
-            this->current_client->res.set_stat_code(METHOD_NOT_ALLOWED);
-        else
-            path_validation();
+    std::cout << READ_S << "--------- Methode: " << current_client->req.getMethod() << READ_E << std::endl;
+    std::cout << READ_S << "--------- Path: " << current_client->req.getPath() << READ_E << std::endl;
+
+    path_validation();  // TOKNOW: auto-index gen
+    
+    std::cout << "STATUS CODE " << current_client->res.get_stat_code() << std::endl;
+    if (this->current_client->res.get_stat_code() != OK) {
+        generate_error_page();
     }
-    response_setup();
+
+    else if (this->current_client->req.getMethod() == GET_METHODE)
+        handle_get();
+
+    else if (this->current_client->req.getMethod() == POST_METHODE)
+        handle_post();
+    
+    else if (this->current_client->req.getMethod() == DELETE_METHODE)
+        handle_delete();
+
+    this->current_client->res.set_raw_response(response_holder);
+    
+    std::cout << GREEN_S << "--------- START RESPONSE\n" << response_holder << "\n------- END RESPONSE" << GREEN_E << std::endl;
 }
