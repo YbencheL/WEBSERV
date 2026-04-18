445eba910b79d1c8d5986c423d18265990a89127
ybenchel <yass515bec@outlook.fr>
2026-04-18 13:42:04 +0100

    fixed the cookie thing

--- cookies_sessions/cookies_session_handler.cpp
+++ cookies_sessions/cookies_session_handler.cpp
@@ -22,7 +22,7 @@ std::vector<std::string> split(std::vector<std::string> keywords, char dilimeter
     return splited_cont;
 }
 
-std::map<std::string, std::string>&  parseCookies(std::string header)
+std::map<std::string, std::string>  parseCookies(std::string header)
 {
     std::string value;
     Session cookie;
@@ -39,7 +39,7 @@ std::map<std::string, std::string>&  parseCookies(std::string header)
     splited_by_semicolon = split(splited_by_space, ';');
     splited_by_equal_sign = split(splited_by_semicolon, '=');
 
-    while(i < splited_by_equal_sign.size())
+    while(i + 1 < splited_by_equal_sign.size())
     {
         // cookie.insert(std::make_pair(splited_by_equal_sign[i], splited_by_equal_sign[i + 1]));
         cookie.SetSessionVal(splited_by_equal_sign[i], splited_by_equal_sign[i + 1]);
