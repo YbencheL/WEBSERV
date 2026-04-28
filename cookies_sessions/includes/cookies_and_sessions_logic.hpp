c73047f41470d4e5e68b19ca5bfc33216f6c68d1
ybenchel <yass515bec@outlook.fr>
2026-04-28 10:47:31 +0100

    mini

--- cookies_sessions/includes/cookies_and_sessions_logic.hpp
+++ cookies_sessions/includes/cookies_and_sessions_logic.hpp
@@ -2,7 +2,7 @@
 #define COOKIES_AND_SESSIONS_LOGIC_HPP
 
 #include "cookies_session_handler.hpp"
-#include "../client.hpp"
+#include "../../client.hpp"
 #include "SessionManager.hpp"
 #include "Session.hpp"   
 
